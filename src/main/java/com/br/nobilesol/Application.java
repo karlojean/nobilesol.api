508268ff05e58cd4181e5846a0452d16da90deac
karlojean <jeankarlodev@gmail.com>
2025-08-05 10:31:07 -0300

    refactor: update application to utm timezone

--- src/main/java/com/br/nobilesol/Application.java
+++ src/main/java/com/br/nobilesol/Application.java
@@ -1,13 +1,22 @@
 package com.br.nobilesol;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
